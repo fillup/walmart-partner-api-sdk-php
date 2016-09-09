90c020cbb281736e72be576fa5a247c923d5ab16
Phillip Shipley <phillip.shipley@gmail.com>
2016-09-09 18:25:48 -0400

    Updated tests to have default dummy consumer id and private key for easier testing

--- tests/PriceTest.php
+++ tests/PriceTest.php
@@ -4,6 +4,7 @@ namespace WalmartTests;
 include __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp\Command\Exception\CommandClientException;
+use Sil\PhpEnv\Env;
 use Walmart\Price;
 
 class PriceTest extends \PHPUnit_Framework_TestCase
@@ -26,8 +27,8 @@ class PriceTest extends \PHPUnit_Framework_TestCase
                     'verify' => $this->verifySsl,
                 ]
             ],
-            'consumerId' => getenv('CONSUMER_ID'),
-            'privateKey' => getenv('PRIVATE_KEY')
+            'consumerId' => Env::get('CONSUMER_ID', 'hw30cqp3-35fi-1bi0-3312-hw9fgm30d2p4'),
+            'privateKey' => Env::get('PRIVATE_KEY', 'MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKzXEfCYdnBNkKAwVbCpg/tR40WixoZtiuEviSEi4+LdnYAAPy57Qw6+9eqJGTh9iCB2wP/I8lWh5TZ49Hq/chjTCPeJiOqi6bvX1xzyBlSq2ElSY3iEVKeVoQG/5f9MYQLEj5/vfTWSNASsMwnNeBbbHcV1S1aY9tOsXCzRuxapAgMBAAECgYBjkM1j1OA9l2Ed9loWl8BQ8X5D6h4E6Gudhx2uugOe9904FGxRIW6iuvy869dchGv7j41ki+SV0dpRw+HKKCjYE6STKpe0YwIm/tml54aNDQ0vQvF8JWILca1a7v3Go6chf3Ib6JPs6KVsUuNo+Yd+jKR9GAKgnDeXS6NZlTBUAQJBANex815VAySumJ/n8xR+h/dZ2V5qGj6wu3Gsdw6eNYKQn3I8AGQw8N4yzDUoFnrQxqDmP3LOyr3/zgOMNTdszIECQQDNIxiZOVl3/Sjyxy9WHMk5qNfSf5iODynv1OlTG+eWao0Wj/NdfLb4pwxRsf4XZFZ1SQNkbNne7+tEO8FTG1YpAkAwNMY2g/ty3E6iFl3ea7UJlBwfnMkGz8rkye3F55f/+UCZcE2KFuIOVv4Kt03m3vg1h6AQkaUAN8acRl6yZ2+BAkEAke2eiRmYANiR8asqjGqr5x2qcm8ceiplXdwrI1kddQ5VUbCTonSewOIszEz/gWp6arLG/ADHOGWaCo8rptAyiQJACXd1ddXUAKs6x3l752tSH8dOde8nDBgF86NGvgUnBiAPPTmJHuhWrmOZmNaB68PsltEiiFwWByGFV+ld9VKmKg=='),
         ];
 
         parent::__construct();
